7d54144d6c09580d414af4a63a17fc2b4df898f1
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-27 14:21:33 +0100

    chore: update vite.config.ts to include web extension plugin and manifest configuration

--- Content-filter/vite.config.ts
+++ Content-filter/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import crx from 'vite-plugin-web-extension'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    crx({
+      manifest: () => ({
+        manifest_version: 3,
+        name: 'Content Filter Extension',
+        version: '1.0.0',
+        permissions: ['storage'],
+        content_scripts: [{
+          matches: ['<all_urls>'],
+          js: ['src/content-script/main.ts'],
+          run_at: 'document_end'
+        }],
+        action: {
+          default_popup: 'index.html'
+        },
+        web_accessible_resources: [{
+          resources: ['model/*'],
+          matches: ['<all_urls>']
+        }]
+      })
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        options: 'options.html'
+      }
+    }
+  }
 })
