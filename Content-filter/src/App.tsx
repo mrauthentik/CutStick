957c8d2443197c810affdeb5d46c47b27263ad68
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-27 09:04:47 +0100

    chore: initialize package.json with dependencies for React, TypeScript, and Webpack

---
+++ Content-filter/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div> 
+      <h1>Content Filter App</h1>
+      <p>This is a simple content filter application.</p>
+    </div>
+  )
+}
+
+export default App;
